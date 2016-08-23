bf76523de77623148ed1be36559c3519850d2af2
Rachel Shaw <rcs8vq@virginia.edu>
2016-08-23 19:29:54 -0400

    Adding home page

---
+++ app/isa_app/home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	context = {
+		'body_text': "Hello, world. This is the main page of the Internet Scale Appliations Project No. 1.",
+	}
+	return render(context, 'home/index.html', request)
