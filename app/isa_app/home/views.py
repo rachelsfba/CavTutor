341d47bdaae18dfc5ec1b43b72a85c1c87edafab
Rachel Shaw <rcs8vq@virginia.edu>
2016-08-23 21:09:38 -0400

    Fixed improper order of arguments to render() method

--- app/isa_app/home/views.py
+++ app/isa_app/home/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
 	context = {
-		'body_text': "Hello, world. This is the main page of the Internet Scale Appliations Project No. 1.",
+		'body_text': 'Hello, world. This is the main page of the Internet Scale Appliations Project No. 1.',
 	}
-	return render(context, 'home/index.html', request)
+	return render(request, 'home/index.html', context)
