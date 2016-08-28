47c8bcddfe7c557bb57aff65b1cf06b56248b096
Andrea Shaw <rshaw@olivermattei.net>
2016-08-28 02:51:47 -0400

    Changed docker-compose and began work on simple User model

--- app/isa_app/home/models.py
+++ app/isa_app/home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    ip_address = models.GenericIPAddressField()
+    times_visited = models.IntegerField(default=0)
