b9203568468bb22207d0f200f78d483f163563e7
Rachel Shaw <rcs8vq@virginia.edu>
2016-08-28 02:51:47 -0400

    Changed docker-compose and began work on simple User model

---
+++ app/isa_app/home/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('ip_address', models.GenericIPAddressField()),
+                ('times_visited', models.IntegerField(default=0)),
+            ],
+        ),
+    ]
