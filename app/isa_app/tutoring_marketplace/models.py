15e91655cce80e99b8bc2c9278d42ca501072986
Andrea Shaw <rshaw@olivermattei.net>
2016-09-15 18:48:02 -0400

    beginning tutoring marketplace app

---
+++ app/isa_app/tutoring_marketplace/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class User:
+    # implement TODO
+
+class Course:
+    # implement TODO
+
+class Tutor:
+    # implement TODO
+
+class Tutee:
+    # implement TODO
+
+class Institution:
+    # implement TODO
+
