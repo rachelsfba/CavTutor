1137bda100c9ac55e971eb131000283de1dfa683
Rachel Shaw <rcs8vq@virginia.edu>
2016-09-16 15:57:51 -0400

    Renamed tutoring_marketplace to placeholder name CavTutor. Also added fixtures to docker-compose instance.

--- app/isa_app/CavTutor/models.py
+++ app/isa_app/CavTutor/models.py
@@ -10,23 +10,35 @@ class User(models.Model):
     password = models.CharField(max_length=256)
     date_joined = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.l_name + ", " + self.f_name
+
 class Course(models.Model):
-    inst_id = models.ForeignKey('Institution', on_delete=models.CASCADE)
+    institution = models.ForeignKey('Institution', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    abbrv = models.CharField(max_length=16)
+    abbr = models.CharField(max_length=16, verbose_name="class abbreviation")
+    instructor = models.CharField(max_length=256)
+    def __str__(self):
+        return self.name + " (" + self.abbr + ")"
 
 class Tutor(models.Model):
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    course_id = models.ForeignKey('Course', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
     adv_rate = models.DecimalField(max_digits=16, decimal_places=2, verbose_name="advertised hourly rate",)
 
+    def __str__(self):
+        return self.user.l_name + ", " + self.user.f_name + " (" + self.course.abbr + ")"
+
 class Tutee(models.Model):
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    course_id = models.ForeignKey('Course', on_delete=models.CASCADE)
-    tutor_id = models.ForeignKey('Tutor', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    tutor = models.ForeignKey('Tutor', on_delete=models.CASCADE)
     # Deciding that this field would probably be better used in a search engine.
     #max_rate = models.DecimalField(max_digits=16, decimal_places=2,
     #        verbose_name="maximum hourly rate willing to pay")
+
+    def __str__(self):
+        return self.user.l_name + ", " + self.user.f_name + " (" + self.course.abbr + ")"
 
 class Institution(models.Model):
     name = models.CharField(max_length=100)
@@ -34,3 +46,5 @@ class Institution(models.Model):
     # in future, subdivide this into many different fields, e.g. street_addr, state_province, country
     address = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.name
--- app/isa_app/isa_app/settings.py
+++ app/isa_app/isa_app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'tutoring_marketplace'
+    'CavTutor',
 )
 
 MIDDLEWARE_CLASSES = (
