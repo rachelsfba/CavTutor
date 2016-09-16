b1dda9f3d0c9e4f3aa9092ba22273a2246693197
Andrea Shaw <rshaw@olivermattei.net>
2016-09-16 16:46:45 -0400

    Added a docker to serve up static files.

--- app/isa_app/isa_app/settings.py
+++ app/isa_app/isa_app/settings.py
@@ -109,4 +109,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = '../static/'
-STATIC_URL = '/static/'
+STATIC_URL = 'http://localhost:81/static/'
