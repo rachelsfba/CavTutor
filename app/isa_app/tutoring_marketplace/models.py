b5a449fe1cf6b2bb0a49c901f898518644298fe9
Rachel Shaw <rcs8vq@virginia.edu>
2016-09-16 02:33:51 -0400

    rough draft of tutoring_marketplace/models.py for now

--- app/isa_app/tutoring_marketplace/models.py
+++ app/isa_app/tutoring_marketplace/models.py
@@ -1,25 +1,36 @@
 from django.db import models
 
 # Create your models here.
-class User:
-    id = models.AutoField(primary_key=True)
+class User(models.Model):
     f_name = models.CharField(max_length=100)
     l_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     username = models.CharField(max_length=32)
-    password = # ??????
-    date_
-class Course:
-    # implement TODO
+    # should define password field more strictly in the future
+    password = models.CharField(max_length=256)
+    date_joined = models.DateTimeField(auto_now=True)
 
-class Tutor:
-    # implement TODO
+class Course(models.Model):
+    inst_id = models.ForeignKey('Institution', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    abbrv = models.CharField(max_length=16)
 
-class Tutee:
-    # implement TODO
+class Tutor(models.Model):
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    course_id = models.ForeignKey('Course', on_delete=models.CASCADE)
+    adv_rate = models.DecimalField(max_digits=16, decimal_places=2, verbose_name="advertised hourly rate",)
 
-class Institution:
-    # implement TODO
+class Tutee(models.Model):
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    course_id = models.ForeignKey('Course', on_delete=models.CASCADE)
+    tutor_id = models.ForeignKey('Tutor', on_delete=models.CASCADE)
+    # Deciding that this field would probably be better used in a search engine.
+    #max_rate = models.DecimalField(max_digits=16, decimal_places=2,
+    #        verbose_name="maximum hourly rate willing to pay")
 
-class Review:
-    #implement
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+    abbrv = models.CharField(max_length=16)
+    # in future, subdivide this into many different fields, e.g. street_addr, state_province, country
+    address = models.CharField(max_length=256)
+
