67f67c49c8f9d76cf1ae2f1cc349c499e3694904
Rachel Shaw <rcs8vq@virginia.edu>
2016-09-20 03:17:01 -0400

    Added Institution services.

--- app/isa_app/CavTutor/urls.py
+++ app/isa_app/CavTutor/urls.py
@@ -1,8 +1,14 @@
+# Handles Services API urls.
 from django.conf.urls import url
 
 from . import views, models, services
 
 urlpatterns = [
+    # User API
     url(r'^api/v1/users/create$', services.User.create),
-    url(r'^api/v1/users/(?P<user_id>\d+)$', services.User.lookup)
+    url(r'^api/v1/users/(?P<user_id>\d+)$', services.User.lookup),
+
+    # Institution API
+    url(r'^api/v1/institutions/create$', services.Institution.create),
+    url(r'^api/v1/institutions/(?P<inst_id>\d+)$', services.Institution.lookup),
 ]
