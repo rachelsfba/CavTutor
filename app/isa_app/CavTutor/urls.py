787d5eee0d8ca1d0569c9da12bc631b6c16de5b8
Andrea Shaw <rshaw@olivermattei.net>
2016-09-20 02:57:07 -0400

    Refactoring following PR 1 to update services API.

--- app/isa_app/CavTutor/urls.py
+++ app/isa_app/CavTutor/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import url
 from . import views, models, services
 
 urlpatterns = [
-    url(r'^api/v1/users/create$', services.create_user),
-    url(r'^api/v1/users/(\d+)$', services.lookup_user)
-    #url(r'^api/v1/(?P<model>[a-z]+)/(?P<action>[a-z0-9]+)$', views.api_handler),
-
+    url(r'^api/v1/users/create$', services.User.create),
+    url(r'^api/v1/users/(?P<user_id>\d+)$', services.User.lookup)
 ]
