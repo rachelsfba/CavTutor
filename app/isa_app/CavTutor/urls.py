77f489d3370b45062dfac50ba53621ebb0c364c8
Rachel Shaw <rcs8vq@virginia.edu>
2016-09-27 17:59:10 -0400

    Removed 'home' app. Updated fixtures to reflect this change.

--- app/isa_app/CavTutor/urls.py
+++ app/isa_app/CavTutor/urls.py
@@ -6,9 +6,11 @@ from . import views, models, services
 urlpatterns = [
     # User API
     url(r'^api/v1/users/create$', services.User.create),
-    url(r'^api/v1/users/(?P<user_id>\d+)$', services.User.lookup),
+    url(r'^api/v1/users/(?P<id>\d+)$', services.User.lookup),
+    url(r'^api/v1/users/delete/(?P<id>\d+)$', services.User.delete),
 
     # Institution API
     url(r'^api/v1/institutions/create$', services.Institution.create),
-    url(r'^api/v1/institutions/(?P<inst_id>\d+)$', services.Institution.lookup),
+    url(r'^api/v1/institutions/(?P<id>\d+)$', services.Institution.lookup),
+#    url(r'^api/v1/institutions/delete/(?P<id>\d+)$', services.Institution.delete),
 ]
