9c923ac0f9f052809196179f4c391975cb6febf5
Andrea Shaw <rshaw@olivermattei.net>
2016-09-27 18:27:46 -0400

    removed additional traces of home app

--- app/isa_app/home/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    ip_address = models.GenericIPAddressField()
-    times_visited = models.IntegerField(default=0)
