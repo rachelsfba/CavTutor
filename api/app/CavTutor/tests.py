768e35a0e3666ece96a3f7ea43ef00d454600f1d
Rachel Shaw <rcs8vq@virginia.edu>
2016-10-02 16:51:06 -0400

    Added a hard-coded update method to Institution model.
    This whole layer should probably be redone. I tried to begin testing the
    views, but because we didn't use Django REST or Form-based views,
    results were discouraging.

--- api/app/CavTutor/tests.py
+++ api/app/CavTutor/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .views import *
 
 # Create your tests here.
+"""
+class InstitutionTestCase(TestCase):
+    def test_create(self):
+        request = RequestFactory().post('/api/v1/institution/create', data={
+                'name': 'University of Cascadia',
+                'abbrv': 'UCA',
+                'address': '123 Seminole Dr',
+                }
+
+        Institution.create
+"""
