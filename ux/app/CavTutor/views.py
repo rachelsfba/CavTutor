740f5a391b7fff6bffb40edddde62431e1facedc
Rachel Shaw <rcs8vq@virginia.edu>
2016-10-06 04:40:02 -0400

    More templating work, plus modifications to readme.

--- ux/app/CavTutor/views.py
+++ ux/app/CavTutor/views.py
@@ -21,8 +21,28 @@ def institution_detail(request, inst_id):
     if request.method != "GET":
         return HttpResponse(statuscode=400)
 
-    json_data = urlopen(API_BASE + 'institutions/{}/?format=json'.format(inst_id)).read()
+    json_data = urlopen(API_BASE + 'institutions/{}/?format=json'.format(inst_id)).read().decode('utf-8')
+    data = json.loads(json_data)
+
+    # add two additional boolean fields to what the API gave us
+    data['num_courses'] = get_institution_num_courses(int(inst_id))
+
+    return HttpResponse(json.dumps(data))
+
+
     return HttpResponse(json_data)
+
+def get_institution_num_courses(inst_id):
+    courses_json = urlopen(API_BASE + 'courses/?format=json').read().decode('utf-8')
+    courses_data = json.loads(courses_json)
+
+    inst_counter = 0
+
+    for course in courses_data:
+        if course['institution'] == inst_id:
+            inst_counter += 1
+
+    return inst_counter
 
 """
     Users
@@ -38,8 +58,36 @@ def user_detail(request, user_id):
     if request.method != "GET":
         return HttpResponse(statuscode=400)
 
-    json_data = urlopen(API_BASE + 'users/{}/?format=json'.format(user_id)).read()
-    return HttpResponse(json_data)
+    json_data = urlopen(API_BASE + 'users/{}/?format=json'.format(user_id)).read().decode('utf-8')
+    data = json.loads(json_data)
+
+    # add two additional boolean fields to what the API gave us
+    data['is_tutor'] = user_is_tutor(int(user_id))
+    data['is_tutee'] = user_is_tutee(int(user_id))
+
+    return HttpResponse(json.dumps(data))
+
+def user_is_tutee(user_id):
+    tutee_json = urlopen(API_BASE + 'tutees/?format=json').read().decode('utf-8')
+    tutee_data = json.loads(tutee_json)
+
+    for record in tutee_data:
+        if record['user'] == user_id:
+            return True
+
+    return False
+
+
+def user_is_tutor(user_id):
+    tutor_json = urlopen(API_BASE + 'tutors/?format=json').read().decode('utf-8')
+    tutor_data = json.loads(tutor_json)
+
+    for record in tutor_data:
+        if record['user'] == user_id:
+            return True
+
+    return False
+
 
 """
     Tutors
