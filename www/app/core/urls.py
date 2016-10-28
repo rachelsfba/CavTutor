fc56a8e2a30e6b2bc3945dc10924e485931ec64f
Andrea Shaw <rshaw@olivermattei.net>
2016-10-28 03:51:43 -0400

    Generified templates the best I could for the www layer.

--- www/app/core/urls.py
+++ www/app/core/urls.py
@@ -18,22 +18,24 @@ from django.contrib import admin
 from CavTutor import views
 
 urlpatterns = [
-    url(r'^institutions/(?P<inst_id>\d+)', views.institution_detail, name='institution-detail'),
-    url(r'^institutions/', views.institution_list, name='institution-list'),
+    url(r'^institutions/(?P<inst_id>\d+)/?', views.institution_detail, name='institution-detail'),
+    url(r'^institutions/?$', views.institution_list, name='institution-list'),
 
-    url(r'^courses/(?P<course_id>\d+)', views.course_detail, name='course-detail'),
-    url(r'^courses', views.course_list, name='course-list'),
+    url(r'^courses/(?P<course_id>\d+)/?$', views.course_detail, name='course-detail'),
+    url(r'^courses/?$', views.course_list, name='course-list'),
 
-    url(r'^users/(?P<user_id>\d+)', views.user_detail, name='user-detail'),
-    url(r'^users', views.user_list, name='user-list'),
+    url(r'^users/(?P<user_id>\d+)/?$', views.user_detail, name='user-detail'),
+    url(r'^users/?$', views.user_list, name='user-list'),
 
-    url(r'^tutors/(?P<tutor_id>\d+)', views.tutor_detail, name='tutor-detail'),
-    url(r'^tutors', views.tutor_list, name='tutor-list'),
+    url(r'^tutors/(?P<tutor_id>\d+)/?$', views.tutor_detail, name='tutor-detail'),
+    url(r'^tutors/?$', views.tutor_list, name='tutor-list'),
 
-    url(r'^tutees/(?P<tutee_id>\d+)', views.tutee_detail, name='tutee-detail'),
-    url(r'^tutees', views.tutee_list, name='tutee-list'),
-    
-    url(r'^login', views.user_login, name='user-login'),    
-    
-    url(r'^$|^index', views.index, name='index'),
+    url(r'^tutees/(?P<tutee_id>\d+)/?$', views.tutee_detail, name='tutee-detail'),
+    url(r'^tutees/?$', views.tutee_list, name='tutee-list'),
+
+    url(r'^login/?$', views.user_login, name='user-login'),
+    url(r'^logout/?$', views.user_logout, name='user-logout'),
+    url(r'^register/?$', views.user_register, name='user-register'),
+
+    url(r'^$|^index/?$', views.index, name='index'),
 ]
