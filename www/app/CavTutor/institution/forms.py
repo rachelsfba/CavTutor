ca08e007362b038dca5f23cc0122934fa8178cc1
Andrea Shaw <rshaw@olivermattei.net>
2016-10-31 03:39:34 -0400

    removing unneeded imports

--- www/app/CavTutor/institution/forms.py
+++ www/app/CavTutor/institution/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class InstitutionNewForm(forms.Form):
+class InstitutionCreateForm(forms.Form):
     name = forms.CharField(label='Name', max_length=100)
     abbr = forms.CharField(label='Abbr', max_length=16)
     address = forms.CharField(label='Address', max_length=256)
--- www/app/CavTutor/institution/views.py
+++ www/app/CavTutor/institution/views.py
@@ -1,66 +1,74 @@
-# views for the Institution model
-from django.views.decorators.csrf import csrf_protect
+"""
+ Namespace: CavTutor.institution.views
+
+ Description: Contains views for the Institution model
+"""
+
+# Allows us to render a template
 from django.shortcuts import render
+
+# Allows us to do reverse lookup URI resolution
 from django.core.urlresolvers import reverse
-from django.http.response import HttpResponse, HttpResponseRedirect
 
-from urllib.request import urlopen
-from urllib.parse import urlencode
-from urllib.error import HTTPError
+# Allow us to perform a redirect instead of rendering a template
+from django.http.response import HttpResponseRedirect
 
+# DRF has a list of HTTP status codes in rest_framework.status
 from rest_framework import status
 
+# Retrieve base experience-layer URL prefix from core.settngs
 from core.settings import UX_BASE 
 
-import json
+# Get our login_required decorator to make things easier on us
 from CavTutor.decorators import login_required
 
-from .forms import *
-import requests
+# Get our forms to create a new record
+from CavTutor.institution.forms import *
+
+# Get some RESTful libraries
+import requests, json
 
 @login_required
 def institution_new(request):
     # Assume we have a good form.
-    status = "ok"
+    state = "ok"
 
     # If the user didn't POST anything, they probably haven't filled out the
     # form yet. Let's give them a blank one to fill in.
     if request.method == 'GET':
         # Create new login form and render it.
-        newinstitution_form = InstitutionNewForm()
+        inst_form = InstitutionCreateForm()
 
     # Otherwise they must have given us something as POST data. Let's try to
     # validate that.
     else:
         # Create a new Django form based on POST data.
-        newinstitution_form = InstitutionNewForm(request.POST)
+        inst_form = InstitutionCreateForm(request.POST)
 
         # If all fields were filled in, let's try to validate that info against
         # our database.
-        if newinstitution_form.is_valid():
+        if inst_form.is_valid():
             # Forms will sanitize for us
-
-            # Redirect to index page after successful new listing
-            # ideally redirect to the new listing itself, (how do you know the url before its created though?)
-            next_page = reverse('index')
 
             # Retrieve login response 
             ux_response = _new_listing_ux(request.POST)
 
             if not ux_response:# or not ux_response['token']:
-                status = "incorrect"
+                state = "incorrect"
             else:
-                #dont need these for a new listing??
-                # auth_cookie = ux_response['token']
-                # expiry = ux_response['expiry_date']
+                # Yay! We succeeded in creating the new record.
+
+                # Redirect to the new object!
+                next_page = reverse('institution-detail', kwargs={"inst_id": ux_response['id']})
+
                 www_response = HttpResponseRedirect(next_page)
                 return www_response
         else:
-            status = "incomplete"
+            state = "incomplete"
 
-    return render(request, 'CavTutor/institution-new.html', {
-            'form': newinstitution_form,
-            'status': status,
+    return render(request, 'CavTutor/institution-create.html', {
+            'form': inst_form,
+            'status': state,
         })
 
     # json_data = urlopen(UX_BASE + 'institutions_new/').read().decode('utf-8')
@@ -73,29 +81,31 @@ def _new_listing_ux(postdata):
     request = requests.post(UX_BASE + 'institutions_new/', data=postdata)
 
 
-    if request.status_code == 201:
+    if request.status_code == status.HTTP_201_CREATED:
         return request.json()
 
     return 
 
 def listings(request):
 
-    json_data = urlopen(UX_BASE + 'institutions/').read().decode('utf-8')
-    context = {'institutions' : json.loads(json_data) }
+    inst_data = requests.get(UX_BASE + 'institutions/')
+
+    context = {'institutions' : inst_data.json() }
 
     return render(request, 'CavTutor/institution-list.html', context)
 
 def detail(request, inst_id):
-    try:
-        json_data = urlopen(UX_BASE + 'institutions/' + inst_id).read().decode('utf-8')
-        context = {'institution' : json.loads(json_data) }
+    
+    inst_data = requests.get(UX_BASE + 'institutions/' + inst_id)
+    
+    if inst_data.status_code == status.HTTP_200_OK:
+        context = {'institution' : inst_data.json() }
 
         return render(request, 'CavTutor/institution-detail.html', context)
 
-    except HTTPError as e:
-        return render(request, 'CavTutor/generics/generic-item-not-found.html', status=404, context={
-                "model": "instititution",
-                "id": inst_id,
-            })
+    return render(request, 'CavTutor/generics/generic-item-not-found.html', status=404, context={
+            "model": "instititution",
+            "id": inst_id,
+        })
 
 # vim: ai ts=4 sts=4 et sw=4
