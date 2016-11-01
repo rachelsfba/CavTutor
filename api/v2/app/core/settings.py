46c5b1ea689bfac595f23f3d9a0536444b403d07
Andrea Shaw <rshaw@olivermattei.net>
2016-11-01 17:31:43 -0400

    Recommitting changes that shouldn't have disappeared ???

--- api/v2/app/core/settings.py
+++ api/v2/app/core/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = '../../static/'
-STATIC_URL = 'http://localhost:8002/static/'
+STATIC_URL = 'http://localhost:8080/static/'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
