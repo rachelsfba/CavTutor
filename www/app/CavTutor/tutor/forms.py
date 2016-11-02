7e50623a74f7dba7757a0ab662e843a6420db47d
Andrea Shaw <rshaw@olivermattei.net>
2016-11-02 15:43:49 -0400

    Refactored some things and added learning/teaching lists for each user.

--- www/app/CavTutor/tutor/forms.py
+++ www/app/CavTutor/tutor/forms.py
@@ -1,32 +1,18 @@
 from django import forms
 from core.settings import UX_BASE 
 
-from urllib.request import urlopen
-from urllib.parse import urlencode
-import json
+import requests, json
+    
+def get_all_courses():
+    courses = requests.get(UX_BASE + 'courses/')
 
+    for course in courses.json():
+        selector_id = course['id']
+        selector_label = "{} at {}".format(course['abbr'], course['institution_name'])
+        
+        yield selector_id, selector_label
 
-max_length=100
-widget=forms.PasswordInput()
-
-def getCourses():
-
-	json_data = urlopen(UX_BASE + 'courses/').read().decode('utf-8')#json_data = requests.get(UX_BASE + 'tutors/').json()
-	dictobj = json.loads(json_data)#return dictobj
-	l=[]
-	for course in dictobj:
-		l.append( (course['id'],course['name']+" ("+course['abbr']+")"+" at "+course['institution_name']))
-	return tuple(l)
-    #MY_COURSES = ( ('1','CS 4144 at UVA'), ('2', 'CS 1501 at VA Tech'))
-    #return MY_COURSES
-
-class TutorRegisterForm(forms.Form):
-	def __init__(self, *args, **kwargs):
-		super(TutorRegisterForm, self).__init__(*args, **kwargs)
-
-		
-		allcourses = getCourses()
-		#MY_COURSES = ( ('1','CS 4144 at UVA'), ('2', 'CS 1501 at VA Tech'))
-		self.fields['course'] = forms.ChoiceField(choices=allcourses)
-
-		self.fields['adv_rate'] = forms.DecimalField(max_digits=16, decimal_places=2)
+class TutorCreateForm(forms.Form):
+    course = forms.ChoiceField(choices=get_all_courses())
+    rate = forms.DecimalField(max_digits=16, decimal_places=2, label="Rate (USD$)")
+    
--- www/app/CavTutor/tutor/views.py
+++ www/app/CavTutor/tutor/views.py
@@ -1,54 +1,38 @@
 # views for the Tutor model
-from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.http.response import HttpResponse, HttpResponseRedirect
 
-from urllib.request import urlopen
-from urllib.parse import urlencode
-from urllib.error import HTTPError
 from rest_framework import status
 
 from CavTutor.decorators import login_required, nologin_required, _get_loggedin_user
-from core.settings import UX_BASE 
 from CavTutor.tutor.forms import *
-import requests
-import json
+
+from core.settings import UX_BASE 
+
+import requests, json
 
 def listings(request):
 
-    json_data = urlopen(UX_BASE + 'tutors/').read().decode('utf-8')
-    context = {'tutors' : json.loads(json_data) }
+    json_data = requests.get(UX_BASE + 'tutors/').json()
+    context = {'tutors' : json_data }
 
     return render(request, 'CavTutor/tutor-list.html', context)
 
 def detail(request, tutor_id):
-    try:
-        json_data = urlopen(UX_BASE + 'tutors/' + tutor_id).read().decode('utf-8')
-        context = {'tutor' : json.loads(json_data) }
+    json_data = requests.get(UX_BASE + 'tutors/' + tutor_id)
+    context = {'tutor' : json_data.json() }
+    
+    if json_data.status_code == status.HTTP_200_OK:
         return render(request, 'CavTutor/tutor-detail.html', context)
-    except HTTPError as e:
-        return render(request, 'CavTutor/generics/generic-item-not-found.html', status=404, context={
-                "model": "tutor",
-                "id": tutor_id,
-            })
 
-# vim: ai ts=4 sts=4 et sw=4
-def _tutor_register_ux(user_id, course_id, adv_rate):
-    data = {
-            'user': user_id,
-            'course': course_id,
-            'adv_rate': adv_rate
-        }
-
-    request = requests.post(UX_BASE + 'tutors/create/', data=data)
-    print(request)
-    if request.status_code == 200 or request.status_code == 201:
-        return request.json()
-    return 
+    return render(request, 'CavTutor/generics/generic-item-not-found.html', status=404, context={
+            "model": "tutor",
+            "id": tutor_id,
+        })
 
 @login_required 
-def tutor_new(request):
+def create(request):
 
     # Assume we have a good form.
     status = "ok"
@@ -57,47 +41,53 @@ def tutor_new(request):
     # form yet. Let's give them a blank one to fill in.
     if request.method == 'GET':
         # Create new login form and render it.
-        register_form = TutorRegisterForm()
+        _form = TutorCreateForm()
 
     # Otherwise they must have given us something as POST data. Let's try to
     # validate that.
     else:
         # Create a new Django form based on POST data.
-        register_form = TutorRegisterForm(request.POST)
+        _form = TutorCreateForm(request.POST)
 
         # If all fields were filled in, let's try to validate that info against
         # our database.
-        if register_form.is_valid():
-            # Redirect to index page after successful login.
-            #next_page = reverse('index')
-            # Retrieve login response 
-            postdata  =dict(request.POST)
+        if _form.is_valid():
+            
+            course = request.POST.get('course')
+            adv_rate = request.POST.get('rate')
 
-            course_id= str(postdata['course'][0])
-            user_id = str(_get_loggedin_user(request)['id'])
-            adv_rate = str(postdata['adv_rate'][0])
+            user = _get_loggedin_user(request)['id']
 
-            ux_register_response = _tutor_register_ux(user_id, course_id, adv_rate)
-            print(ux_register_response)
-            if ux_register_response:
-                next_page = reverse('tutor-detail', kwargs={"tutor_id": ux_register_response['id']})
+            ux_response = _tutor_register_ux(user, course, adv_rate)
+            
+            if ux_response:
+                next_page = reverse('tutor-detail', kwargs={"tutor_id": ux_response['id']})
+                
+                return HttpResponseRedirect(next_page)
             else:
-                next_page = reverse('tutor-create')
-
-            if not ux_register_response:
                 # ux layer said the form was invalid;
-                # probably means a user already exists with that username or email
+                # probably means that tutor record already exists
                 status = "invalid" 
-            else:
-                www_response = HttpResponseRedirect(next_page)
-                return www_response
-                #return render(request, 'CavTutor/index.html', {
-                #        'username': request.POST.get('username'),
-                #   })
+
         else:
             status = "incomplete"
 
-    return render(request, 'CavTutor/tutor-register.html', {
-            'form': register_form,
+    return render(request, 'CavTutor/tutor-create.html', {
+            'form': _form,
             'status': status,
         })
+
+def _tutor_register_ux(user_id, course_id, adv_rate):
+    data = {
+            'user': user_id,
+            'course': course_id,
+            'adv_rate': adv_rate
+        }
+
+    request = requests.post(UX_BASE + 'tutors/create/', data=data)
+
+    if request.status_code == 201:
+        return request.json()
+    return 
+
+# vim: ai ts=4 sts=4 et sw=4
