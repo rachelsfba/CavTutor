4a22535bc68e57b0f5a0cd3d3453f56b4fc42e47
Andrea Shaw <rshaw@olivermattei.net>
2016-11-08 01:06:55 -0500

    Got everything except user-facing www docker running on a basic level. Still need to create a search result page that uses the tutor_views.search results.

--- ux/app/CavTutor/tutor/views.py
+++ ux/app/CavTutor/tutor/views.py
@@ -1,7 +1,7 @@
 """
     MODULE:
     CavTutor.tutor.views
-    
+
     DESCRIPTION:
     Acts as a go-between for the user-facing and API layers for Tutor objects.
 """
@@ -9,14 +9,14 @@
 """ We need these libraries to parse the API layer's JSON responses into Python
     data structures, as well as to update the database through sending data back
     to the API layer. """
-import requests, json 
+import requests, json
 
 """ These libraries are needed for cookie token generation. """
 import os, hmac
 
 """ We need to get the API_BASE prefix from the settings file so that we can
     access the API information. """
-from core.settings import API_BASE, UX_BASE
+from core.settings import API_BASE, UX_BASE, ELASTIC_SEARCH_NUM_RESULTS
 
 """  We utilize some common Django idioms, so fetch those implementations. """
 from django.shortcuts import render
@@ -27,22 +27,61 @@ from django.contrib.auth.hashers import check_password, make_password
     having to write our own. """
 from rest_framework import status
 
+""" We want to add new listings to a Kafka queue. """
+from kafka import KafkaProducer
+producer = KafkaProducer(bootstrap_servers='kafka:9092')
+
+""" We want to search all the tutor listings. """
+from elasticsearch import Elasticsearch
+elasticsearch = Elasticsearch([{'host': 'elasticsearch'}])
+
+
 # List of all tutors
 def listings(request):
     if request.method != "GET":
         return HttpResponseBadRequest()
-    
+
     tutor_data = requests.get(API_BASE + 'tutors/?format=json')
-    
+
     if tutor_data.status_code != status.HTTP_200_OK:
         return HttpResponseNotFound()
-    
+
     tutor_data_parsed = []
 
     for tutor in tutor_data.json():
         tutor_data_parsed.append(_tutor_foreign_key_id_to_json(tutor))
-    
+
     return HttpResponse(json.dumps(tutor_data_parsed))
+
+def search(request):
+
+    if request.method != "POST" or not request.POST.get('query'):
+        return HttpResponseBadRequest()
+
+    query_str = request.POST.get('query')
+
+    search_result = elasticsearch.search(
+            index='tutor-listing-indexer',
+            body={
+                'query': {
+                    'query_string': {
+                        'query': query_str,
+                        }
+                    },
+                # Implement pagination algorithm at some point (e.g. first 25, second 25, etc. on page 1, 2, ...)
+                'size': ELASTIC_SEARCH_NUM_RESULTS,
+            })
+
+    tutors_found = []
+
+    for tutor in search_result['hits']['hits']:
+        if '_source' in tutor:
+            tutor_json = _tutor_foreign_key_id_to_json(tutor['_source'])
+            tutors_found.append(tutor_json)
+
+    # {'timed_out': False, 'hits': {'total': 1, 'hits': [{'_score': 0.10848885, '_index': 'listing_index', '_source': {'id': 42, 'description': 'This is a used Macbook Air in great condition', 'title': 'Used MacbookAir 13"'}, '_id': '42', '_type': 'listing'}], 'max_score': 0.10848885}, '_shards': {'successful': 5, 'total': 5, 'failed': 0}, 'took': 21}
+    return HttpResponse(json.dumps(tutors_found))
+
 
 # Details a specific tutor
 def detail(request, tutor_id):
@@ -53,19 +92,23 @@ def detail(request, tutor_id):
 
     if json_data.status_code != status.HTTP_200_OK:
         return HttpResponseNotFound()
-    
+
     tutor_data = _tutor_foreign_key_id_to_json(json_data.json())
     tutor_data['num_tutees'] = get_tutor_num_tutees(tutor_id)
 
     return HttpResponse(json.dumps(tutor_data))
 
 def _tutor_foreign_key_id_to_json(tutor):
-    user_data = requests.get(UX_BASE + 'users/{}/'.format(tutor['user'])) 
+    user_data = requests.get(UX_BASE + 'users/{}/'.format(tutor['user']))
     course_data = requests.get(UX_BASE + 'courses/{}/'.format(tutor['course']))
-    
+
     tutor['user'] = user_data.json()
     tutor['course'] = course_data.json()
-    
+
+    # don't even THINK about giving the web layer a password without it
+    # explicitly requiring it!~
+    del tutor['user']['password']
+
     return tutor
 
 def create(request):
@@ -84,7 +127,7 @@ def create(request):
     for tutor in tutor_list.json():
         user_matches  = request.POST.get('user') == str(tutor['user'])
         course_matches = request.POST.get('course') == str(tutor['course'])
-        
+
         if user_matches and course_matches:
             # uh-oh, it already exists in system
             return HttpResponseBadRequest()
@@ -93,20 +136,23 @@ def create(request):
     new_tutor_data = requests.post(API_BASE + 'tutors/', data=request.POST)
     if new_tutor_data.status_code != 201:
         return HttpResponseServerError()
+
+    producer.send('new-tutor-listing-topic', new_tutor_data.content) # N.B. .content returns bytes instead of text
     return HttpResponse(new_tutor_data.text, status=201)
 
 def get_tutor_num_tutees(tutor_id):
     tutee_data = requests.get(API_BASE + 'tutees/?format=json')
-    
+
     #can't return 404 from here :C
     #if tutor_data.status_code != status.HTTP_200_OK:
     #    return HttpResponseNotFound()
-    
+
     tutee_counter = 0
-    
+
     for tutee in tutee_data.json():
         if tutee['tutor'] == int(tutor_id):
             tutee_counter += 1
-    
+
     return tutee_counter
+
 
