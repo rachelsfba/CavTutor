f22e2992db70880d433a0057216ace2a80103167
Andrea Shaw <rshaw@olivermattei.net>
2016-11-08 18:41:27 -0500

    working on making elasticsearch querying more robust

--- search/indexer.py
+++ search/indexer.py
@@ -8,6 +8,11 @@ elasticsearch = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
 
 for message in consumer:
     new_tutor = json.loads(message.value.decode('utf-8'))
+    
+    
+    print(message)
+    
+    
     ret = elasticsearch.index(index='tutor-listing-indexer', doc_type='listing', id=new_tutor['id'], body=new_tutor)
     elasticsearch.indices.refresh(index="tutor-listing-indexer")
 
