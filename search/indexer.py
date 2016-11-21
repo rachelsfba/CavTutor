8975ef9cb8af5400752521b7b6c10fc48d17fbe8
Andrea Shaw <rshaw@olivermattei.net>
2016-11-21 02:25:35 -0500

    Working on bugfixing with tutor creating

--- search/indexer.py
+++ search/indexer.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
+
+""" Get kafka-python library loaded. """
 from kafka import KafkaConsumer
+
+""" Load ElasticSearch library. """
 from elasticsearch import Elasticsearch
+
+""" JSON and requests libraries. """
 import json, requests
-
-def handle_requests():
-    consumer = KafkaConsumer(
-            group_id='tutor-listing-indexer',
-            bootstrap_servers=['kafka:9092'],
-            value_deserializer=lambda v: json.loads(v.decode('utf-8')),
-        )
-
-    consumer.subscribe(['new-tutor-listing-topic',])
-    elasticsearch = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
-
-    while not elasticsearch.indices.exists('tutor-listing-indexer'):
-        elasticsearch.indices.create(index='tutor-listing-indexer', ignore=400)
-
-    dequeue(consumer, elasticsearch)
-
 
 def dequeue(consumer, elasticsearch):
     for message in consumer:
-        #new_tutor = json.loads(message.value.decode('utf-8'))
-        new_tutor = message.value
 
-        elasticsearch.index(index='tutor-listing-indexer', doc_type='listing', id=new_tutor['id'], body=new_tutor)
+        try:
+            new_tutor = json.loads(message.value.decode('utf-8'))
+            elasticsearch.index(index='tutor-listing-indexer', doc_type='listing', id=new_tutor['id'], body=new_tutor)
+        except ValueError:
+            continue
+
         elasticsearch.indices.refresh(index="tutor-listing-indexer")
 
-try:
-    handle_requests()
-except:
-    pass
+consumer = KafkaConsumer(
+        group_id='tutor-listing-indexer',
+        bootstrap_servers=['kafka:9092'],
+    )
+
+
+consumer.subscribe(['new-tutor-listing-topic',])
+elasticsearch = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
+
+print("---1---")
+
+while not elasticsearch.indices.exists('tutor-listing-indexer'):
+    elasticsearch.indices.create(index='tutor-listing-indexer', ignore=400)
+
+print("---2---")
+dequeue(consumer, elasticsearch)
+print("---3---")
