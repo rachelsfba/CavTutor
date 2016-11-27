ccded869c9a0b7fe51302dd4c35171626d77530c
Andrea Shaw <rshaw@olivermattei.net>
2016-11-27 02:13:16 -0500

    Trying to fix ux crashing in deployment.

--- ux/deploy_server.sh
+++ ux/deploy_server.sh
@@ -9,7 +9,7 @@ WSGI_FILE=$DJANGO_BASE/core/wsgi.py
 
 # Loads fixtures from API layer into Kafka queue
 # uses dockerize to wait until api and kafka are up and ready
-dockerize -wait tcp://api:8000 \
+dockerize -timeout 30s -wait tcp://api:8000 \
     -wait tcp://kafka:9092 \
     python $DJANGO_BASE/load_fixtures.py && \
     mod_wsgi-express start-server --reload-on-changes --log-to-terminal --working-directory $DJANGO_BASE $WSGI_FILE
