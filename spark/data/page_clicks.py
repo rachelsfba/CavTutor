093895eb6f90bdcc110bf2957cac9332096e83cf
Daniel Saha <drs5ma@virginia.edu>
2016-12-08 15:54:41 -0500

    coviews working

--- spark/data/page_clicks.py
+++ spark/data/page_clicks.py
@@ -1,10 +1,8 @@
-import os
 from pyspark import SparkContext
 
-sc = SparkContext(os.environ.get("MASTER"), "PopularItems")
+sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
-# each worker loads a piece of the data file
-data = sc.textFile(os.environ.get("SPARK_INPUT_FILE"), 2)
+data = sc.textFile("/tmp/data/input.txt", 2)     # each worker loads a piece of the data file
 
 
 
@@ -13,30 +11,65 @@ pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split ea
 
 users = pairs.reduceByKey(lambda x,y: ([y] if type(y)==str else y)+ ([x] if type(x)==str else x) )
 users = users.map(lambda x: (x[0],[x[1]]) if type(x[1])==str else x)
-users = users.map(lambda x: (x[0],x[1][::-1])) #reverse into chrono order
-
-# users = [
+users_list = users.map(lambda x: (x[0],x[1][::-1])) #reverse into chrono order
+# users_list = [
 #    ("alice", (1,2,3,4) )
 #    ("bob", (3,4,5,1) )
-#    (name, list of pages visited)
+#    (name, list of pages visited in order)
 #]
 
 
-for i in users.collect():
-    print("users ", i)
+for i in users_list.collect():
+    print("users_list ", i)
+
+def split_into_pairs(userlist):
+    l = []
+    for pairnum in range(len(userlist[1])-1):
+        c =  (userlist[1][pairnum], userlist[1][pairnum+1])
+        l.append((userlist[0],c))    
+    return l
+
+
+users_pairs = users_list.flatMap(split_into_pairs)
+# users_pairs = [
+#  ("alice", (1,2)),
+#  ("alice", (2,3)),
+#  ("bob", (2,3)),
+#  (name, pair of consecutive pages user visited)
+#]
+
+
+#if Bob went from (4,5) more than one time lets just say he only did it once.....
+users_pairs = users_pairs.distinct()
+
+
+for i in users_pairs.collect():
+    print("users_pairs ", i)
+
+pairs_users = users_pairs.map(lambda x: (x[1],x[0])) 
+for i in pairs_users.collect():
+    print("pairs_users", i)
+# same as users_pairs but key and val are switched
+
+
+pairs_lists = pairs_users.reduceByKey(lambda x,y: ([y] if type(y)==str else y)+ ([x] if type(x)==str else x) )
+pairs_lists = pairs_lists.map(lambda x: (x[0],[x[1]]) if type(x[1])==str else x)
+for i in pairs_lists.collect():
+    print("pairs_lists", i)
+
+pairs_counts = pairs_lists.map(lambda x: (x[0], len(x[1]) ) )
+for i in pairs_counts.collect():
+    print("pairs_counts", i)
 
 
 
+print("Related Items (considering 3 or more distinct users) ")
+pairs_threeormore = pairs_counts.filter(lambda x: True if x[1]>=3 else False)
+for i in pairs_threeormore.collect():
+    print("Item: ", i[0], "Count: ", i[1])
 
 
-pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
-count = pages.reduceByKey(lambda x,y: x+y)        # shuffle the data so that each key is only on one worker
-                                                  # and then reduce all the values by adding them together
-
-output = count.collect()                          # bring the data back to the master node so we can print it out
-for page_id, count in output:
-    print ("page_id %s count %d" % (page_id, count))
-print ("Popular items done")
+print ("Coviews computed.")
 
 
 
