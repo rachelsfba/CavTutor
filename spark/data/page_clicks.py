6768d6fd7072e2fafc7727b084f6f8a88df6d763
Daniel Saha <drs5ma@virginia.edu>
2016-12-08 15:02:18 -0500

    begin pySpark script, data_generator to python2.7

--- spark/data/page_clicks.py
+++ spark/data/page_clicks.py
@@ -4,7 +4,29 @@ sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
 data = sc.textFile("/tmp/data/input.txt", 2)     # each worker loads a piece of the data file
 
+
+
 pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split each line of it's partition
+
+
+users = pairs.reduceByKey(lambda x,y: ([y] if type(y)==str else y)+ ([x] if type(x)==str else x) )
+users = users.map(lambda x: (x[0],[x[1]]) if type(x[1])==str else x)
+users = users.map(lambda x: (x[0],x[1][::-1])) #reverse into chrono order
+
+# users = [
+#    ("alice", (1,2,3,4) )
+#    ("bob", (3,4,5,1) )
+#    (name, list of pages visited)
+#]
+
+
+for i in users.collect():
+    print("users ", i)
+
+
+
+
+
 pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
 count = pages.reduceByKey(lambda x,y: x+y)        # shuffle the data so that each key is only on one worker
                                                   # and then reduce all the values by adding them together
@@ -13,5 +35,13 @@ output = count.collect()                          # bring the data back to the m
 for page_id, count in output:
     print ("page_id %s count %d" % (page_id, count))
 print ("Popular items done")
+
+
+
+
+
+
+
+
 
 sc.stop()
